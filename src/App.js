776e6ae032fcb2b7e58801fcb329d5e730f8d696
hiroto-hash <hiroto5118@icloud.com>
2020-12-02 12:44:28 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     const dom = <h1>hi!</h1>
-//     return (
-//       <React.Fragment>
-//         { dom}
-//         <input type="text" onChange={() => { console.log('Hi Mista') }} />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const App = () => {
+  //配列
+  const profile = [
+    { name:"mista",age:"20"},
+    { name:"dio",age:"22"},
+    { name:"zo"}
+  ];
   return(
     <div>
-      <Cat />
-      <Cat />
+      {
+        // const profileの配列の要素文出力 + keyを付けてuniqueにしてあげるindex付けてあげればok
+        profile.map((profile,index) => {
+          return <User name={profile.name} age={profile.age}  key={index}/>
+        })
+      }
+      <User name={"Hanako"} age="10" />
     </div>
   )
 };
 
-const Cat = () => {
-  return <div>Hi!</div>
+//profile（親）からデータをpropsで受け取ってその中身を表示している
+const User = (props) => {
+return <div>Hi, I am { props.name }, and { props.age } years old!</div>
+}
+
+//propsの中に要素が無い時にはdefaultとしてこの値が入る
+User.defaultProps = {
+  age:1
 }
 export default App;
