06ee8e999f2499b087a47eee4d6cc6f4a3830438
hiroto-hash <hiroto5118@icloud.com>
2020-12-02 12:28:53 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    const dom = <h1>hi!</h1>
-    return (
-      <React.Fragment>
-        { dom}
-        <input type="text" onChange={() => { console.log('Hi Mista') }} />
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     const dom = <h1>hi!</h1>
+//     return (
+//       <React.Fragment>
+//         { dom}
+//         <input type="text" onChange={() => { console.log('Hi Mista') }} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+    </div>
+  )
+};
+
+const Cat = () => {
+  return <div>Hi!</div>
 }
-
 export default App;
