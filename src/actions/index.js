ab3d01fe27098b68dec16ad7c86fbcdc2496e889
hiroto-hash <hiroto5118@icloud.com>
2020-12-02 14:02:59 +0900

    create action

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+//acion作成(オブジェクトのなかでtypeというkeyをもつ)
+export const increment = () => ({
+  type: 'INCREMENT' 
+})
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
