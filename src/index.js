04ac9fcc96328d71401ec71932f062de0bc01b8b
hiroto-hash <hiroto5118@icloud.com>
2020-12-03 13:04:04 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 import reducer from './reducers';
-import EventsIndex from './components/event_index';
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 //thnk->アクションの代わりに関数返せる
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store = {store}>
-    <EventsIndex />
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path ='/events/new' component={EventsNew}></Route>
+        <Route exact path ='/' component={EventsNew}></Route>
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
